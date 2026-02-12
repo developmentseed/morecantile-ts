f2fe53281d4e2926c323a532452ad70fb9ae8d5e
Kyle Barron <kyle@developmentseed.org>
2026-02-12 13:15:16 -0500

    Helper methods for matrix-wide and tile-specific geotransforms (#9)
    * Create per-matrix and per-tile transforms
    
    * Clean up transform test
    
    * recursive

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+export type { Affine } from "./transform";
+export { matrixTransform, tileTransform } from "./transform";
 export type {
   BoundingBox,
   CRS,
   TileMatrix,
   TileMatrixSet,
 } from "./types/index";
-
 export { metersPerUnit } from "./utils";
